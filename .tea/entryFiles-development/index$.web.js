9e25d4e877bfcce76567a6cdded8933b5f899504
sammymutahigicheru <sammymutahi12@gmail.com>
2020-12-03 11:41:28 +0300

    add search bar

--- .tea/entryFiles-development/index$.web.js
+++ .tea/entryFiles-development/index$.web.js
@@ -1,4 +1,5 @@
 require('@alipay/appx-compiler/lib/sjsEnvInit');
 require('./config$');
 
-require('../../pages/pullLoading/index?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
+require('../../node_modules/mini-antui/es/search-bar/index?hash=05d2a9730dd6009bf9446182f9c985f40f8c0f43');
+require('../../pages/pullLoading/index?hash=c8bb201eecc8e16d00b6ee8469ccde5158d099ef');
--- .tea/entryFiles-development/index$.worker.js
+++ .tea/entryFiles-development/index$.worker.js
@@ -32,7 +32,8 @@ if(AFAppX.registerApp) {
 
 function success() {
 require('../../app');
-require('../../pages/pullLoading/index?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
+require('../../node_modules/mini-antui/es/search-bar/index?hash=05d2a9730dd6009bf9446182f9c985f40f8c0f43');
+require('../../pages/pullLoading/index?hash=c8bb201eecc8e16d00b6ee8469ccde5158d099ef');
 }
 self.bootstrapApp ? self.bootstrapApp({ success }) : success();
 }
